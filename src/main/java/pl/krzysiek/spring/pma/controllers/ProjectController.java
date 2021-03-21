70af7177cb0bde2b105286696914871850c1e08f
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-03-21 18:02:02 +0100

    project controller

---
+++ src/main/java/pl/krzysiek/spring/pma/controllers/ProjectController.java
@@ -0,0 +1,21 @@
+package pl.krzysiek.spring.pma.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.krzysiek.spring.pma.entities.Project;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+    @GetMapping("/new")
+    public String displayProjectForm(Model model){
+
+        Project aProject = new Project();
+        model.addAttribute("project", aProject);
+
+        return"new-project";
+    }
+}
